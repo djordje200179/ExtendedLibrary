27e2f99274185e875e61bcab1b010629a19dae91
Đorđe Milanović <djordje200179@gmail.com>
2022-06-08 15:50:45 +0200

    Event type improved

---
+++ concurrency/event/event.go
@@ -0,0 +1,25 @@
+package event
+
+import "github.com/djordje200179/GoExtendedLibrary/concurrency/semaphore"
+
+type Event struct {
+	sem semaphore.Semaphore
+}
+
+func New() Event {
+	return Event{semaphore.New(0)}
+}
+
+func (event Event) Wait() {
+	event.sem.Wait()
+}
+
+func (event Event) Notify() {
+	event.sem.Signal()
+}
+
+func (event Event) NotifyAll() {
+	for event.sem.Value() > 0 {
+		event.sem.Signal()
+	}
+}
