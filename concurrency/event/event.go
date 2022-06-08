263a8e5b3126b1eb7e438969f537b36b571c7d4a
Đorđe Milanović <djordje200179@gmail.com>
2022-06-08 15:53:53 +0200

    Event methods fixed

--- concurrency/event/event.go
+++ concurrency/event/event.go
@@ -10,15 +10,15 @@ func New() Event {
 	return Event{semaphore.New(0)}
 }
 
-func (event Event) Wait() {
+func (event *Event) Wait() {
 	event.sem.Wait()
 }
 
-func (event Event) Notify() {
+func (event *Event) Notify() {
 	event.sem.Signal()
 }
 
-func (event Event) NotifyAll() {
+func (event *Event) NotifyAll() {
 	for event.sem.Value() > 0 {
 		event.sem.Signal()
 	}
