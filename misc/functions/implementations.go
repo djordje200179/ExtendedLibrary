44d60b62477c71a8b4b2108b41a4fccfda91c2bb
Đorđe Milanović <djordje200179@gmail.com>
2022-06-09 20:18:08 +0200

    More functions implemented

--- misc/functions/implementations.go
+++ misc/functions/implementations.go
@@ -4,6 +4,10 @@ func Increment(i int) int {
 	return i + 1
 }
 
-func Smaller(a, b int) bool {
+func NumsAscending(a, b int) bool {
 	return a < b
+}
+
+func NumsDescending(a, b int) bool {
+	return a > b
 }
