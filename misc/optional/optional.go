9f2ead6e9fc4b81f44cbc77ef1f300880da69dc5
Đorđe Milanović <djordje200179@gmail.com>
2022-06-09 18:16:40 +0200

    Optional type implemented

---
+++ misc/optional/optional.go
@@ -0,0 +1,43 @@
+package optional
+
+type Optional[T any] struct {
+	value T
+	valid bool
+}
+
+func Empty[T any]() Optional[T] {
+	return Optional[T]{
+		valid: false,
+	}
+}
+
+func FromValue[T any](value T) Optional[T] {
+	return New[T](value, true)
+}
+
+func New[T any](value T, valid bool) Optional[T] {
+	return Optional[T]{
+		value: value,
+		valid: valid,
+	}
+}
+
+func (o Optional[T]) IsPresent() bool {
+	return o.valid
+}
+
+func (o Optional[T]) GetOrElse(other T) T {
+	if o.valid {
+		return o.value
+	} else {
+		return other
+	}
+}
+
+func (o Optional[T]) GetOrPanic() T {
+	if o.valid {
+		return o.value
+	} else {
+		panic("No value present")
+	}
+}
