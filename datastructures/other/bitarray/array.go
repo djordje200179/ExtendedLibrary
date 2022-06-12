9fb5ea1aebe91ab7daabb30e12952c8431deebe5
Đorđe Milanović <djordje200179@gmail.com>
2022-06-13 00:45:54 +0200

    Bitarray partially implemented

--- datastructures/other/bitarray/array.go
+++ datastructures/other/bitarray/array.go
@@ -1,8 +1,8 @@
 package bitarray
 
 import (
+	"fmt"
 	"github.com/djordje200179/extendedlibrary/datastructures/sequences/array"
-	"github.com/djordje200179/extendedlibrary/streams"
 )
 
 type BitArray struct {
@@ -22,41 +22,56 @@ func (b *BitArray) Size() int {
 }
 
 func (b *BitArray) Get(index int) bool {
-	elementIndex := index / 8
-	elementOffset := index % 8
-	element := b.array.Get(elementIndex)
+	if index >= b.Size() {
+		panic(fmt.Sprintf("runtime error: index out of range [%d] with length %d", index, b.Size()))
+	}
 
-	return element & (1 << elementOffset)
+	elemIndex := index / 8
+	elemOff := index % 8
+	elem := b.array.Get(elemIndex)
+	masked := elem & (1 << elemOff)
+
+	return masked != 0
 }
 
 func (b *BitArray) Set(index int, value bool) {
-	elementIndex := index / 8
-	elementOffset := index % 8
-	element := b.array.Get(elementIndex)
-
-	if value {
-		element |= 1 << elementOffset
-	} else {
-		element &= ^(1 << elementOffset)
+	if index >= b.Size() {
+		panic(fmt.Sprintf("runtime error: index out of range [%d] with length %d", index, b.Size()))
 	}
 
-	b.array.Set(elementIndex, element)
+	elemIndex := index / 8
+	elemOff := index % 8
+	elem := b.array.Get(elemIndex)
+
+	mask := uint8(1) << elemOff
+	if value {
+		elem |= mask
+	} else {
+		elem &= ^mask
+	}
+
+	b.array.Set(elemIndex, elem)
 }
 
 func (b *BitArray) Append(value bool) {
-	elementIndex := b.array.Size() - 1
-	elementOffset := b.lastElemOff
+	if b.lastElemOff == 0 {
+		b.array.Append(0)
+	}
 
+	b.lastElemOff = (b.lastElemOff + 1) % 8
+
+	b.Set(b.Size()-1, value)
 }
 
 func (b *BitArray) Insert(index int, value bool) {
+	elemIndex := index / 8
+	elemOff := index % 8
+	elem := b.array.Get(elemIndex)
 
 }
 
 func (b *BitArray) Remove(index int) bool {
-
-}
-
-func (b *BitArray) Stream() streams.Stream[bool] {
-
+	elemIndex := index / 8
+	elemOff := index % 8
+	elem := b.array.Get(elemIndex)
 }
