c3cd8dfe6ea69889c6367d18dc3b3fccfe7957bb
Đorđe Milanović <djordje200179@gmail.com>
2022-06-15 20:00:50 +0200

    Group collector implemented

--- streams/collector/group.go
+++ streams/collector/group.go
@@ -1,38 +1,26 @@
 package collector
 
-//func Group[T any, P comparable](stream Stream[T], mapper functions.Mapper[T, P]) Stream[misc.Pair[P, []T]] {
-//	ret := create[misc.Pair[P, []T]]()
-//
-//	go func() {
-//		if !ret.waitRequest() {
-//			ret.close()
-//			return
-//		}
-//
-//		m := make(map[P][]T)
-//		stream.ForEach(func(data T) {
-//			key := mapper(data)
-//
-//			_, ok := m[key]
-//			if !ok {
-//				m[key] = []T{}
-//			}
-//
-//			m[key] = append(m[key], data)
-//		})
-//
-//		entries := make([]misc.Pair[P, []T], 0, len(m))
-//		for key, value := range m {
-//			entries = append(entries, misc.Pair[P, []T]{key, value})
-//		}
-//
-//		ret.data <- entries[0]
-//		for i := 1; i < len(entries) && ret.waitRequest(); i++ {
-//			ret.data <- entries[i]
-//		}
-//
-//		ret.close()
-//	}()
-//
-//	return ret
-//}
+import (
+	"github.com/djordje200179/extendedlibrary/misc/functions"
+)
+
+type group[T any, K comparable] struct {
+	m      map[K][]T
+	mapper functions.Mapper[T, K]
+}
+
+func Group[T any, K comparable](mapper functions.Mapper[T, K]) Collector[T, map[K][]T] {
+	return group[T, K]{
+		m:      make(map[K][]T),
+		mapper: mapper,
+	}
+}
+
+func (group group[T, K]) Supply(value T) {
+	key := group.mapper(value)
+	group.m[key] = append(group.m[key], value)
+}
+
+func (group group[T, K]) Finish() map[K][]T {
+	return group.m
+}
