ddd3a9310a5a9ec49c376c0c7e1fc58ac792cb15
Đorđe Milanović <djordje200179@gmail.com>
2022-06-15 19:54:04 +0200

    Stringify collector implemented

--- streams/collector/stringify.go
+++ streams/collector/stringify.go
@@ -1,1 +1,31 @@
 package collector
+
+import (
+	"fmt"
+	"strings"
+)
+
+type stringify[T any] struct {
+	builder   *strings.Builder
+	delimiter string
+}
+
+func Stringify[T any](delimiter string) Collector[T, string] {
+	return stringify[T]{
+		builder:   new(strings.Builder),
+		delimiter: delimiter,
+	}
+}
+
+func (stringify stringify[T]) Supply(value T) {
+	if stringify.builder.Len() > 0 {
+		stringify.builder.WriteString(stringify.delimiter)
+	}
+
+	str := fmt.Sprint(value)
+	stringify.builder.WriteString(str)
+}
+
+func (stringify stringify[T]) Finish() string {
+	return stringify.builder.String()
+}
