08d831658d45674e624dacb6e605d817f17ee214
Đorđe Milanović <djordje200179@gmail.com>
2022-06-20 17:24:10 +0200

    Test temporarily removed

--- testing/test.go
+++ testing/test.go
@@ -1,8 +1,5 @@
 package main
 
-import "github.com/djordje200179/extendedlibrary/datastructures/sequences/linkedlist"
-
 func main() {
-	list := linkedlist.New[int]()
-	list.Stream().ForEach(func(item int) { println(item) })
+
 }
