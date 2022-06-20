27c6bc0c09138b6e6cbcd19fe73e49194cd99858
Đorđe Milanović <djordje200179@gmail.com>
2022-06-20 16:59:07 +0200

    Messenger type improved

--- concurrency/messenger/messenger.go
+++ concurrency/messenger/messenger.go
@@ -3,61 +3,59 @@ package messenger
 import (
 	"github.com/djordje200179/extendedlibrary/misc/functions"
 	"github.com/djordje200179/extendedlibrary/misc/optional"
+	"unsafe"
 )
 
-type Messenger[T any] struct {
-	ch     chan T
-	closed bool
+type Messenger[T any] chan T
+
+func New[T any](bufferSize int) Messenger[T] {
+	return make(chan T, bufferSize)
 }
 
-func New[T any]() *Messenger[T] {
-	return &Messenger[T]{
-		ch:     make(chan T),
-		closed: false,
-	}
+func (messenger Messenger[T]) Send(signal T) {
+	messenger <- signal
 }
 
-func (messenger *Messenger[T]) Send(signal T) {
-	messenger.ch <- signal
-}
-
-func (messenger *Messenger[T]) ReadSync() optional.Optional[T] {
-	if messenger.closed {
+func (messenger Messenger[T]) ReadSync() optional.Optional[T] {
+	if messenger.Closed() {
 		return optional.Empty[T]()
 	} else {
-		return optional.FromValue(<-messenger.ch)
+		return optional.FromValue(<-messenger)
 	}
 }
 
-func (messenger *Messenger[T]) ReadAsync(callback functions.ParamCallback[T]) {
+func (messenger Messenger[T]) ReadAsync(callback functions.ParamCallback[T]) {
 	go func() {
-		if messenger.closed {
+		if messenger.Closed() {
 			return
 		}
 
-		value := <-messenger.ch
+		value := <-messenger
 		callback(value)
 	}()
 }
 
-func (messenger *Messenger[T]) ReadIfHasData() optional.Optional[T] {
-	if messenger.closed {
+func (messenger Messenger[T]) ReadIfHasData() optional.Optional[T] {
+	if messenger.Closed() {
 		return optional.Empty[T]()
 	}
 
 	select {
-	case data := <-messenger.ch:
+	case data := <-messenger:
 		return optional.FromValue(data)
 	default:
 		return optional.Empty[T]()
 	}
 }
 
-func (messenger *Messenger[T]) Closed() bool {
-	return messenger.closed
+func (messenger Messenger[T]) Closed() bool {
+	cptr := *(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&messenger)) + unsafe.Sizeof(uint(0))))
+	cptr += unsafe.Sizeof(uint(0)) * 2
+	cptr += unsafe.Sizeof(unsafe.Pointer(uintptr(0)))
+	cptr += unsafe.Sizeof(uint16(0))
+	return *(*uint32)(unsafe.Pointer(cptr)) > 0
 }
 
-func (messenger *Messenger[T]) Close() {
-	messenger.closed = true
-	close(messenger.ch)
+func (messenger Messenger[T]) Close() {
+	close(messenger)
 }
