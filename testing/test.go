abf220dfa273b3ddeb0041ff98cfd735eae809d5
Đorđe Milanović <djordje200179@gmail.com>
2022-06-21 21:00:25 +0200

    Testing improved

--- testing/test.go
+++ testing/test.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/djordje200179/extendedlibrary/misc/functions"
+	"github.com/djordje200179/extendedlibrary/streams"
+)
 
+func main() {
+	streams.
+		Generate(1, functions.Increment).
+		Filter(func(value int) bool { return value%6 == 0 }).
+		Seek(5).
+		Limit(10).
+		ForEach(func(value int) { fmt.Println(value) })
 }
