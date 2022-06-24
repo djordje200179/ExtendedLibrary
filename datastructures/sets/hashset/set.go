87705a5e600ea790074f0b990fa138f6729745f3
Đorđe Milanović <djordje200179@gmail.com>
2022-06-24 13:40:00 +0200

    Set type improved

--- datastructures/sets/hashset/set.go
+++ datastructures/sets/hashset/set.go
@@ -7,50 +7,51 @@ import (
 	"github.com/djordje200179/extendedlibrary/streams"
 )
 
-type Set[T comparable] struct {
-	m hashmap.Map[T, bool]
-}
+type Set[T comparable] hashmap.Map[T, bool]
 
-func New[T comparable]() Set[T] {
-	return Set[T]{hashmap.New[T, bool]()}
-}
+func New[T comparable]() Set[T] { return Set[T](hashmap.New[T, bool]()) }
 
 func Collector[T comparable]() streams.Collector[T, sets.Set[T]] {
 	return sets.Collector[T](New[T]())
 }
 
+func (set Set[T]) m() hashmap.Map[T, bool] {
+	return hashmap.Map[T, bool](set)
+}
+
 func (set Set[T]) Size() int {
-	return set.m.Size()
+	return set.m().Size()
 }
 
 func (set Set[T]) Add(value T) {
 	if !set.Contains(value) {
-		set.m.Set(value, true)
+		set.m().Set(value, true)
 	}
 }
 
 func (set Set[T]) Remove(value T) {
-	set.m.Remove(value)
+	set.m().Remove(value)
 }
 
 func (set Set[T]) Contains(value T) bool {
-	return set.m.Contains(value)
+	return set.m().Contains(value)
 }
 
 func (set Set[T]) Clear() {
-	set.m.Clear()
+	set.m().Clear()
 }
 
 func (set Set[T]) Clone() sets.Set[T] {
-	return Set[T]{set.m.Clone().(hashmap.Map[T, bool])}
+	cloned := set.m().Clone().(hashmap.Map[T, bool])
+	return Set[T](cloned)
 }
 
 func (set Set[T]) Iterator() datastructures.Iterator[T] {
-	return iterator[T]{set.m.Iterator()}
+	return iterator[T]{set.m().Iterator()}
 }
 
 func (set Set[T]) Stream() *streams.Stream[T] {
-	it := set.m.Iterator()
+	it := set.m().Iterator()
 
 	return streams.Supply(func() T {
 		defer it.Move()
--- datastructures/sets/linkedlistset/set.go
+++ datastructures/sets/linkedlistset/set.go
@@ -7,30 +7,30 @@ import (
 	"github.com/djordje200179/extendedlibrary/streams"
 )
 
-type Set[T comparable] struct {
-	list *linkedlist.LinkedList[T]
-}
+type Set[T comparable] linkedlist.LinkedList[T]
 
-func New[T comparable]() Set[T] {
-	return Set[T]{linkedlist.New[T]()}
-}
+func New[T comparable]() *Set[T] { return (*Set[T])(linkedlist.New[T]()) }
 
 func Collector[T comparable]() streams.Collector[T, sets.Set[T]] {
 	return sets.Collector[T](New[T]())
 }
 
-func (set Set[T]) Size() int {
-	return set.list.Size()
+func (set *Set[T]) list() *linkedlist.LinkedList[T] {
+	return (*linkedlist.LinkedList[T])(set)
 }
 
-func (set Set[T]) Add(value T) {
+func (set *Set[T]) Size() int {
+	return set.list().Size()
+}
+
+func (set *Set[T]) Add(value T) {
 	if !set.Contains(value) {
-		set.list.Append(value)
+		set.list().Append(value)
 	}
 }
 
-func (set Set[T]) Remove(value T) {
-	for it := set.list.ModifyingIterator(); it.Valid(); it.Move() {
+func (set *Set[T]) Remove(value T) {
+	for it := set.list().ModifyingIterator(); it.Valid(); it.Move() {
 		if it.Get() == value {
 			it.Remove()
 			return
@@ -38,8 +38,8 @@ func (set Set[T]) Remove(value T) {
 	}
 }
 
-func (set Set[T]) Contains(value T) bool {
-	for it := set.list.Iterator(); it.Valid(); it.Move() {
+func (set *Set[T]) Contains(value T) bool {
+	for it := set.list().Iterator(); it.Valid(); it.Move() {
 		if it.Get() == value {
 			return true
 		}
@@ -48,18 +48,19 @@ func (set Set[T]) Contains(value T) bool {
 	return false
 }
 
-func (set Set[T]) Clear() {
-	set.list.Clear()
+func (set *Set[T]) Clear() {
+	set.list().Clear()
 }
 
-func (set Set[T]) Clone() sets.Set[T] {
-	return Set[T]{set.list.Clone().(*linkedlist.LinkedList[T])}
+func (set *Set[T]) Clone() sets.Set[T] {
+	cloned := set.list().Clone().(*linkedlist.LinkedList[T])
+	return (*Set[T])(cloned)
 }
 
-func (set Set[T]) Iterator() datastructures.Iterator[T] {
-	return set.list.Iterator()
+func (set *Set[T]) Iterator() datastructures.Iterator[T] {
+	return set.list().Iterator()
 }
 
-func (set Set[T]) Stream() *streams.Stream[T] {
-	return set.list.Stream()
+func (set *Set[T]) Stream() *streams.Stream[T] {
+	return set.list().Stream()
 }
