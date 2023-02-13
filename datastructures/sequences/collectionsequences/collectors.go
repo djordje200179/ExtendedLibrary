c7127d5dcb0e30b4d6aa17310fd8ff85d8c2ca86
Đorđe Milanović <djordje200179@gmail.com>
2023-02-13 14:36:05 +0100

    Redundant functions removed

--- datastructures/sequences/collectionsequences/collectors.go
+++ datastructures/sequences/collectionsequences/collectors.go
@@ -5,14 +5,8 @@ import (
 	"github.com/djordje200179/extendedlibrary/streams"
 )
 
-func QueueCollector[T any]() streams.Collector[T, sequences.Queue[T]] {
+func Collector[T any]() streams.Collector[T, sequences.Queue[T]] {
 	return sequences.Collector[T, sequences.Queue[T]]{
-		BackPusher: NewDeque[T](),
-	}
-}
-
-func StackCollector[T any]() streams.Collector[T, sequences.Stack[T]] {
-	return sequences.Collector[T, sequences.Stack[T]]{
 		BackPusher: NewDeque[T](),
 	}
 }
