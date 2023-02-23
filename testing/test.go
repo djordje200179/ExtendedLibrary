f3a014706ed1354c875f9e91a57c6a2adc39ce3f
Đorđe Milanović <djordje200179@gmail.com>
2023-02-23 19:06:45 +0100

    BST fixed

--- testing/test.go
+++ testing/test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/djordje200179/extendedlibrary/datastructures/maps/binarysearchtree"
 	"github.com/djordje200179/extendedlibrary/misc/functions/comparison"
 )
@@ -11,6 +12,10 @@ func main() {
 	tree.Set(1, "one")
 	tree.Set(5, "five")
 	tree.Set(3, "three")
+	tree.Set(2, "four")
+	tree.Set(3, "three-2")
 
-	for node := tree.M
+	for node := tree.Root().Min(); node != nil; node = node.Next() {
+		fmt.Println(node.Key(), node.Value)
+	}
 }
