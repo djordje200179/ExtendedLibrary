6256aa4a4f186775af3ad637a53b191687f779ac
Đorđe Milanović <djordje200179@gmail.com>
2023-02-23 19:26:32 +0100

    Buffer queue implemented

---
+++ datastructures/sequences/boundedbuffer/buffer.go
@@ -0,0 +1,32 @@
+package boundedbuffer
+
+import (
+	"github.com/djordje200179/extendedlibrary/datastructures/sequences"
+	"github.com/djordje200179/extendedlibrary/streams"
+)
+
+type Buffer[T any] chan T
+
+func New[T any](size int) Buffer[T] {
+	return make(chan T, size)
+}
+
+func Collector[T any]() streams.Collector[T, sequences.Queue[T]] {
+	return sequences.Collector[T, sequences.Queue[T]]{New[T]()}
+}
+
+func (buffer Buffer[T]) Empty() bool {
+	return len(buffer) == 0
+}
+
+func (buffer Buffer[T]) PushBack(value T) {
+	buffer <- value
+}
+
+func (buffer Buffer[T]) PopFront() T {
+	return <-buffer
+}
+
+func (buffer Buffer[T]) PeekFront() T {
+	panic("Not supported")
+}
