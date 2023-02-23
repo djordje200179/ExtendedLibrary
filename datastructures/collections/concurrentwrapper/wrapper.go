99f5f663c4766bfaf05568566004c55416547623
Đorđe Milanović <djordje200179@gmail.com>
2023-02-23 17:39:35 +0100

    Wrapper improved and fixed

--- datastructures/collections/concurrentwrapper/wrapper.go
+++ datastructures/collections/concurrentwrapper/wrapper.go
@@ -9,109 +9,105 @@ import (
 )
 
 type Wrapper[T any] struct {
-	collections.Collection[T]
+	collection collections.Collection[T]
 
 	mutex sync.RWMutex
+}
+
+func From[T any](collection collections.Collection[T]) Wrapper[T] {
+	return Wrapper[T]{collection, sync.RWMutex{}}
 }
 
 func (wrapper *Wrapper[T]) Size() int {
 	wrapper.mutex.RLock()
 	defer wrapper.mutex.RUnlock()
 
-	return wrapper.Collection.Size()
+	return wrapper.collection.Size()
 }
 
 func (wrapper *Wrapper[T]) Get(index int) T {
 	wrapper.mutex.RLock()
 	defer wrapper.mutex.RUnlock()
 
-	return wrapper.Collection.Get(index)
+	return wrapper.collection.Get(index)
 }
 
 func (wrapper *Wrapper[T]) GetRef(index int) *T {
 	wrapper.mutex.RLock()
 	defer wrapper.mutex.RUnlock()
 
-	return wrapper.Collection.GetRef(index)
+	return wrapper.collection.GetRef(index)
 }
 
 func (wrapper *Wrapper[T]) Set(index int, value T) {
 	wrapper.mutex.RLock()
 	defer wrapper.mutex.RUnlock()
 
-	wrapper.Collection.Set(index, value)
+	wrapper.collection.Set(index, value)
 }
 
-func (wrapper *Wrapper[T]) Append(value T) {
+func (wrapper *Wrapper[T]) Append(values ...T) {
 	wrapper.mutex.Lock()
 	defer wrapper.mutex.Unlock()
 
-	wrapper.Collection.Append(value)
+	wrapper.collection.Append(values...)
 }
 
-func (wrapper *Wrapper[T]) AppendMany(values ...T) {
+func (wrapper *Wrapper[T]) Insert(index int, values ...T) {
 	wrapper.mutex.Lock()
 	defer wrapper.mutex.Unlock()
 
-	wrapper.Collection.Append(values...)
-}
-
-func (wrapper *Wrapper[T]) Insert(index int, value T) {
-	wrapper.mutex.Lock()
-	defer wrapper.mutex.Unlock()
-
-	wrapper.Collection.Insert(index, value)
+	wrapper.collection.Insert(index, values...)
 }
 
 func (wrapper *Wrapper[T]) Remove(index int) {
 	wrapper.mutex.Lock()
 	defer wrapper.mutex.Unlock()
 
-	wrapper.Collection.Remove(index)
+	wrapper.collection.Remove(index)
 }
 
 func (wrapper *Wrapper[T]) Clear() {
 	wrapper.mutex.Lock()
 	defer wrapper.mutex.Unlock()
 
-	wrapper.Collection.Clear()
+	wrapper.collection.Clear()
 }
 
 func (wrapper *Wrapper[T]) Reverse() {
 	wrapper.mutex.Lock()
 	defer wrapper.mutex.Unlock()
 
-	wrapper.Collection.Reverse()
+	wrapper.collection.Reverse()
 }
 
 func (wrapper *Wrapper[T]) Swap(index1, index2 int) {
 	wrapper.mutex.Lock()
 	defer wrapper.mutex.Unlock()
 
-	wrapper.Collection.Swap(index1, index2)
+	wrapper.collection.Swap(index1, index2)
 }
 
 func (wrapper *Wrapper[T]) Sort(comparator functions.Comparator[T]) {
 	wrapper.mutex.Lock()
 	defer wrapper.mutex.Unlock()
 
-	wrapper.Collection.Sort(comparator)
+	wrapper.collection.Sort(comparator)
 }
 
 func (wrapper *Wrapper[T]) Join(other collections.Collection[T]) {
 	wrapper.mutex.Lock()
 	defer wrapper.mutex.Unlock()
 
-	wrapper.Collection.Join(other)
+	wrapper.collection.Join(other)
 }
 
 func (wrapper *Wrapper[T]) Clone() collections.Collection[T] {
 	wrapper.mutex.RLock()
 	defer wrapper.mutex.RUnlock()
 
-	return &Wrapper[T]{
-		Collection: wrapper.Collection.Clone(),
-	}
+	clonedCollection := wrapper.collection.Clone()
+	return &Wrapper[T]{clonedCollection, sync.RWMutex{}}
 }
 
 func (wrapper *Wrapper[T]) Iterator() iterable.Iterator[T] {
@@ -120,15 +116,15 @@ func (wrapper *Wrapper[T]) Iterator() iterable.Iterator[T] {
 
 func (wrapper *Wrapper[T]) ModifyingIterator() collections.Iterator[T] {
 	return iterator[T]{
-		Iterator: wrapper.Collection.ModifyingIterator(),
+		Iterator: wrapper.collection.ModifyingIterator(),
 		mutex:    &wrapper.mutex,
 	}
 }
 
 func (wrapper *Wrapper[T]) Stream() streams.Stream[T] {
-	return wrapper.Collection.Stream()
+	return wrapper.collection.Stream()
 }
 
 func (wrapper *Wrapper[T]) RefStream() streams.Stream[*T] {
-	return wrapper.Collection.RefStream()
+	return wrapper.collection.RefStream()
 }
