e7f76fcc5e3a97e01c2263b9ff50f3dccb2f0611
Đorđe Milanović <djordje200179@gmail.com>
2023-04-28 20:47:57 +0200

    First type annotations added

--- datastructures/iterable/iterable.go
+++ datastructures/iterable/iterable.go
@@ -1,12 +1,14 @@
 package iterable
 
+// An Iterator fits well with iteration through for loop.
 type Iterator[T any] interface {
-	Valid() bool
-	Move()
+	Valid() bool // Valid returns false if the iterator is exhausted.
+	Move()       // Move moves the iterator to the next element.
 
-	Get() T
+	Get() T // Get returns the current element.
 }
 
+// An Iterable is a collection of elements that can be iterated through.
 type Iterable[T any] interface {
-	Iterator() Iterator[T]
+	Iterator() Iterator[T] // Iterator returns an iterator for the collection.
 }
