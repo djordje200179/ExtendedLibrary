922cbfd8d154287c8a76dc02ade2748ea43133a3
Đorđe Milanović <djordje200179@gmail.com>
2023-05-14 01:03:54 +0200

    Misc types improved

--- misc/optional/optional.go
+++ misc/optional/optional.go
@@ -1,6 +1,7 @@
 package optional
 
 import (
+	"fmt"
 	"github.com/djordje200179/extendedlibrary/misc/functions"
 	"github.com/djordje200179/extendedlibrary/misc/functions/predication"
 )
@@ -8,6 +9,10 @@ import (
 type Optional[T any] struct {
 	Value T
 	Valid bool
+}
+
+func New[T any](value T, valid bool) Optional[T] {
+	return Optional[T]{value, valid}
 }
 
 func Empty[T any]() Optional[T] {
@@ -20,39 +25,47 @@ func FromValue[T any](value T) Optional[T] {
 	return Optional[T]{value, true}
 }
 
-func (o Optional[T]) GetOrElse(other T) T {
-	if o.Valid {
-		return o.Value
+func (optional Optional[T]) GetOrElse(other T) T {
+	if optional.Valid {
+		return optional.Value
 	} else {
 		return other
 	}
 }
 
-func (o Optional[T]) GetOrDefault() T {
+func (optional Optional[T]) GetOrDefault() T {
 	var def T
-	return o.GetOrElse(def)
+	return optional.GetOrElse(def)
 }
 
-func (o Optional[T]) Process(onValue functions.ParamCallback[T], onEmpty functions.EmptyCallback) {
-	if o.Valid {
-		onValue(o.Value)
+func (optional Optional[T]) Process(onValue functions.ParamCallback[T], onEmpty functions.EmptyCallback) {
+	if optional.Valid {
+		onValue(optional.Value)
 	} else {
 		onEmpty()
 	}
 }
 
-func Map[T any, P any](o Optional[T], mapper functions.Mapper[T, P]) Optional[P] {
-	if o.Valid {
-		return FromValue(mapper(o.Value))
+func Map[T any, P any](optional Optional[T], mapper functions.Mapper[T, P]) Optional[P] {
+	if optional.Valid {
+		return FromValue(mapper(optional.Value))
 	} else {
 		return Empty[P]()
 	}
 }
 
-func (o Optional[T]) Filter(predicate predication.Predictor[T]) Optional[T] {
-	if o.Valid && predicate(o.Value) {
-		return o
+func (optional Optional[T]) Filter(predicate predication.Predictor[T]) Optional[T] {
+	if optional.Valid && predicate(optional.Value) {
+		return optional
 	} else {
 		return Empty[T]()
+	}
+}
+
+func (optional Optional[T]) String() string {
+	if optional.Valid {
+		return fmt.Sprint(optional.Value)
+	} else {
+		return "(empty)"
 	}
 }
--- misc/pair.go
+++ misc/pair.go
@@ -1,8 +1,14 @@
 package misc
 
+import "fmt"
+
 type Pair[T1 any, T2 any] struct {
 	First  T1
 	Second T2
 }
 
-func (p Pair[T1, T2]) Get() (T1, T2) { return p.First, p.Second }
+func (pair Pair[T1, T2]) Get() (T1, T2) { return pair.First, pair.Second }
+
+func (pair Pair[T1, T2]) String() string {
+	return fmt.Sprintf("(%v, %v)", pair.First, pair.Second)
+}
