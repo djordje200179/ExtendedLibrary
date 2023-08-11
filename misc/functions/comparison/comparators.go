25a2b73a582310171188a97900b8bce3b33f1e02
Đorđe Milanović <djordje200179@gmail.com>
2023-08-11 15:26:42 +0200

    New comparator added

---
+++ misc/functions/comparison/comparators.go
@@ -0,0 +1,15 @@
+package comparison
+
+import (
+	"cmp"
+	"github.com/djordje200179/extendedlibrary/misc/functions"
+)
+
+func NewFieldComparator[T any, P cmp.Ordered](getter functions.Mapper[T, P]) Comparator[T] {
+	return func(first, second T) int {
+		firstValue := getter(first)
+		secondValue := getter(second)
+
+		return cmp.Compare(firstValue, secondValue)
+	}
+}
