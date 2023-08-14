902a2469c6378e7263161ee0cdd961fce6f5273b
Đorđe Milanović <djordje200179@gmail.com>
2023-08-14 23:56:49 +0200

    Waiting for task simplified

--- executors/action.go
+++ executors/action.go
@@ -1,46 +1,20 @@
 package executors
 
-import "sync"
-
 type Action struct {
 	function func()
 
-	wg      sync.WaitGroup
-	started bool
-	done    bool
+	*taskWaiter
 }
 
 func NewAction(function func()) *Action {
 	action := &Action{
-		function: function,
+		function:   function,
+		taskWaiter: newTaskWaiter(),
 	}
-
-	action.wg.Add(1)
 
 	return action
 }
 
 func (action *Action) Function() func() {
 	return action.function
-}
-
-func (action *Action) Wait() {
-	action.wg.Wait()
-}
-
-func (action *Action) IsStarted() bool {
-	return action.started
-}
-
-func (action *Action) IsDone() bool {
-	return action.done
-}
-
-func (action *Action) MarkStarted() {
-	action.started = true
-}
-
-func (action *Action) MarkDone() {
-	action.done = true
-	action.wg.Done()
 }
--- executors/future.go
+++ executors/future.go
@@ -1,23 +1,18 @@
 package executors
 
-import "sync"
-
 type Future[T any] struct {
 	getter func() (T, error)
 	result T
 	err    error
 
-	wg      sync.WaitGroup
-	started bool
-	done    bool
+	*taskWaiter
 }
 
 func NewFuture[T any](getter func() (T, error)) *Future[T] {
 	future := &Future[T]{
-		getter: getter,
+		getter:     getter,
+		taskWaiter: newTaskWaiter(),
 	}
-
-	future.wg.Add(1)
 
 	return future
 }
@@ -29,13 +24,13 @@ func NewErrorlessFuture[T any](getter func() T) *Future[T] {
 }
 
 func (future *Future[T]) TryGet() (T, error) {
-	future.wg.Wait()
+	future.Wait()
 
 	return future.result, future.err
 }
 
 func (future *Future[T]) Get() T {
-	future.wg.Wait()
+	future.Wait()
 
 	if future.err != nil {
 		panic(future.err)
@@ -48,21 +43,4 @@ func (future *Future[T]) Function() func() {
 	return func() {
 		future.result, future.err = future.getter()
 	}
-}
-
-func (future *Future[T]) IsStarted() bool {
-	return future.started
-}
-
-func (future *Future[T]) IsDone() bool {
-	return future.done
-}
-
-func (future *Future[T]) MarkStarted() {
-	future.started = true
-}
-
-func (future *Future[T]) MarkDone() {
-	future.done = true
-	future.wg.Done()
 }
