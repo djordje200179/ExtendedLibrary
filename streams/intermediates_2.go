8552f06df867b30f6ad3f307752427c0fdf154ef
Đorđe Milanović <djordje200179@gmail.com>
2024-02-21 20:58:25 +0100

    Methods reorganized

--- streams/intermediates_2.go
+++ streams/intermediates_2.go
@@ -2,6 +2,26 @@ package streams
 
 import "github.com/djordje200179/extendedlibrary/misc/functions/predication"
 
+func (s Stream2[K, V]) Keys() Stream[K] {
+	return func(yield func(K) bool) {
+		for k, _ := range s {
+			if !yield(k) {
+				break
+			}
+		}
+	}
+}
+
+func (s Stream2[K, V]) Values() Stream[V] {
+	return func(yield func(V) bool) {
+		for _, v := range s {
+			if !yield(v) {
+				break
+			}
+		}
+	}
+}
+
 func (s Stream2[K, V]) FilterKeys(predicate predication.Predicate[K]) Stream2[K, V] {
 	return func(yield func(K, V) bool) {
 		for key, value := range s {
--- streams/stream_2.go
+++ streams/stream_2.go
@@ -11,23 +11,3 @@ type Streamer2[K, V any] interface {
 func From2[K, V any](seq iter.Seq2[K, V]) Stream2[K, V] {
 	return Stream2[K, V](seq)
 }
-
-func (s Stream2[K, V]) Keys() Stream[K] {
-	return func(yield func(K) bool) {
-		for k, _ := range s {
-			if !yield(k) {
-				break
-			}
-		}
-	}
-}
-
-func (s Stream2[K, V]) Values() Stream[V] {
-	return func(yield func(V) bool) {
-		for _, v := range s {
-			if !yield(v) {
-				break
-			}
-		}
-	}
-}
